2368aea60b885b1ded40f14f0c6ef870c12bb0c3
bitfurry <sahilkharb@iitj.ac.in>
2015-11-09 15:36:07 +0530

    adjacent locations will give all the adjacent locations of all the locations

--- lib/game_of_life/locations.rb
+++ lib/game_of_life/locations.rb
@@ -6,6 +6,18 @@ module GameOfLife
       @locations = locations
     end
 
+    def adjacent_locations
+      adj_locs = []
+      self.locations.each do |location|
+        tmp_adjacent_locations = location.adjacent_locations
+        tmp_adjacent_locations.each do |tmp_location|
+          adj_locs << tmp_location
+        end
+      end
+      adj_locs = adj_locs.uniq
+      adj_locs
+    end
+
     def ==(other)
       if other.is_a?(self.class)
         other.locations == self.locations
--- spec/game_of_life/locations_spec.rb
+++ spec/game_of_life/locations_spec.rb
@@ -2,10 +2,28 @@ require  'spec_helper'
 
 module GameOfLife
   describe Locations do
+
     locations = [Location.new(1, 2), Location.new(2, 2), Location.new(2, 1), Location.new(2, 0), Location.new(1, 0), Location.new(0, 0),
                  Location.new(0, 1), Location.new(0, 2)]
     other_locations = [Location.new(3, 2), Location.new(2, 2), Location.new(2, 1), Location.new(2, 0), Location.new(1, 0), Location.new(0, 0),
                  Location.new(2, 1), Location.new(0, 2)]
+
+    def build_adj_locations(locations)
+      ans = []
+      locations.each do |loc|
+        tmp = loc.adjacent_locations
+        tmp.each do |tmp_loc|
+          ans << tmp_loc
+        end
+      end
+      ans = ans.uniq
+      ans
+    end
+
+      locations1 = Locations.new(locations)
+      it 'adjacent locations should give adjacent locations of all the locations' do
+        expect(locations1.adjacent_locations).to match_array(build_adj_locations(locations))
+      end
 
     context 'equality' do
       locations1 = Locations.new(locations)
