de931c0d03ded68dc2bfb112ee681911769f98d2
bitfurry <sahilkharb@iitj.ac.in>
2015-11-09 14:56:24 +0530

    locations with same locations are equal

--- lib/game_of_life/locations.rb
+++ lib/game_of_life/locations.rb
@@ -1,5 +1,11 @@
 module GameOfLife
   class Locations
+    attr_reader :locations
+
+    def initialize(locations)
+      @locations = locations
+    end
+
     def ==(other)
       if other.is_a?(self.class)
         true
--- spec/game_of_life/locations_spec.rb
+++ spec/game_of_life/locations_spec.rb
@@ -2,16 +2,24 @@ require  'spec_helper'
 
 module GameOfLife
   describe Locations do
+    locations = [Location.new(1, 2), Location.new(2, 2), Location.new(2, 1), Location.new(2, 0), Location.new(1, 0), Location.new(0, 0),
+                 Location.new(0, 1), Location.new(0, 2)]
+
     context 'equality' do
-      locations = Locations.new
+      locations1 = Locations.new(locations)
+      locations2 = Locations.new(locations)
       obj = Object.new
 
       it 'should be equal to itself' do
-        expect(locations).to eq(locations)
+        expect(locations1).to eq(locations1)
       end
 
       it 'should not be compared with other than a locations' do
-        expect(locations).not_to eq(obj)
+        expect(locations1).not_to eq(obj)
+      end
+
+      it 'should be equal if all the locations are equal' do
+        expect(locations1).to eq(locations2)
       end
     end
   end
