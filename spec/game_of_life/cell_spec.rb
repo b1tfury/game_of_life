add7463cad984398aab6c80b94d54b81f17f32cd
bitfurry <sahilkharb@iitj.ac.in>
2015-11-09 12:52:49 +0530

    context of cell birth added

--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -2,12 +2,14 @@ require 'spec_helper'
 
 module GameOfLife
   describe Cell do
-    it 'should create an alive cell' do
-      expect(Cell.new_alive_cell.is_alive?).to eq(true)
-    end
+    context 'cell birth' do
+      it 'should create an alive cell' do
+        expect(Cell.new_alive_cell.is_alive?).to eq(true)
+      end
 
-    it 'should create a dead cell' do
-      expect(Cell.new_dead_cell.is_dead?).to eq(true)
+      it 'should create a dead cell' do
+        expect(Cell.new_dead_cell.is_dead?).to eq(true)
+      end
     end
   end
 end
